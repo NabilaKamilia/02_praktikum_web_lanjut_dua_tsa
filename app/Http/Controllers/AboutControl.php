248a5386caefea8c2fc7be88982dd9374fa5fdc9
= <=>
2022-09-05 21:07:23 +0700

    pembaruan

---
+++ app/Http/Controllers/AboutControl.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutControl extends Controller
+{
+    public function about(){
+        return "1941720158 - Nabila Kamilia";
+    }
+}
